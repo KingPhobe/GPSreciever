c923e2cc09d0e2e9214cd2e35e80ed91eb761c05
KingPhobe <bsmatan@gmail.com>
2026-02-10 01:11:46 +0200

    Add unified integrity report

--- gnss_twin/integrity/__init__.py
+++ gnss_twin/integrity/__init__.py
@@ -2,6 +2,7 @@
 """Integrity monitoring tools."""
 
 from gnss_twin.integrity.flags import IntegrityConfig, SvTracker, integrity_pvt
-from gnss_twin.integrity.raim import IntegrityReport, compute_raim, raim_fde
+from gnss_twin.integrity.raim import compute_raim, raim_fde
+from gnss_twin.integrity.report import IntegrityReport
 
 __all__ = ["IntegrityConfig", "SvTracker", "IntegrityReport", "compute_raim", "integrity_pvt", "raim_fde"]
